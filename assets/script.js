46b47f55c5a7809d974ba8bf8610446a74d61f24
Andrew An <andyan7@gmail.com>
2022-01-23 17:25:46 -0800

    Added container class and appended to HTML, attempted local storage save and retrieve

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,34 @@ var hourly = moment().format('H');
 for(var i = 6; i < 18; i++) {
     var hourlyPlan = itineraryTime.add(1, 'hour').format('hh:mm A')
     var ItineraryState;
+
+if (hourly < i) {
+    ItineraryState = 'past';
+} else if (hourly == i) {
+    ItineraryState = 'present';
+} else if (hourly > i) {
+    ItineraryState = 'future';
 }
 
-if (hour < i) {
-    ItineraryState = '.past';
-} else if (hour == i) {
-    ItineraryState = '.present';
-} else if (hour > i) {
-    ItineraryState = '.future';
-}
+var containerClasses =
+`<container class="row" id='hour-${i}'>
+<div class="col-2"></div>
+<div class="hour w-25 p-4 col-1">${hourlyPlan}</div>
+<textarea class="description w-50 p-4 col-6 ${ItineraryState} hour-${i}"></textarea>
+<button class="save w-25 p-4 col-1 fas fa-save fa-2x"></button>
+<div class="col-2"></div>  
+</container>
+<p></p>`;
 
-var 
+$("#all").append(containerClasses);
+};
+
+$('.save').on('click', function () {
+    var valSib = $(this).siblings('.description').val();
+    var timePar = $(this).parent().attr('id');
+    localStorage.setItem(timePar, valSib);
+});
+
+// for (var i = 6; i < 18; i++) {
+//     $('#hour-${i}').val(localStorage.getItem('hour-${i}'));
+// }
