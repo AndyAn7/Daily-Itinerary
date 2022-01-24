7119a90e60dde8a97940cf3dbd1b73898caaedd1
Andrew An <andyan7@gmail.com>
2022-01-23 16:14:10 -0800

    Added moment variables along with for loop

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
 $('#currentDay').text(moment().format('MMM Do YYYY'));
+
+var itineraryTime = moment().startOf('day').add(12, 'hour');
+var hourly = moment().format('H');
+
+for(var i = 6; i < 18; i++) {
+    var hourlyPlan = itineraryTime.add(1, 'hour').format('hh:mm A')
+    var ItineraryState;
+}
